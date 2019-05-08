71f5c981697b9c8fd3432a391fcb73d181dd682a
KFC <kfchang@fstop.com.tw>
2019-05-08 18:06:22 +0800

    Adding the Signin Functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,15 +26,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5cce91db8331f9a18235c8d3')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
       //res.setHeader('Set-Cookie', 'loggedIn=true;Max-Age=10');
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
