498f5b35aaeea67530a6e32c300f861273bd3741
KFC <kfchang@fstop.com.tw>
2019-05-08 21:14:35 +0800

    Finishing the Flash Messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,10 +10,17 @@ exports.getLogin = (req, res, next) => {
   //     .trim()
   //     .split('=')[1] === 'true';
   console.log(req.session.isLoggedIn);
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash('error')
+    errorMessage: message
   });
 };
 
